05f9f14843632df27043f998f3ab70a635c20ef8
dr1998 <rodriguezdavid089@gmail.com>
2016-05-24 12:06:19 -0600

    Update Pruebas.java

--- pruebas/src/pruebas/Pruebas.java
+++ pruebas/src/pruebas/Pruebas.java
@@ -18,6 +18,7 @@ public class Pruebas {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         JOptionPane.showMessageDialog(null,"hola amigos ");
+        System.out.printIn("Actualizacion2");
     }
     
 }
